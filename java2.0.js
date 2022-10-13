a4e09e8ee95694c88f2e0b0c5eaaf418921ccf49
EthanSchultz <ethanschultz0224@gmail.com>
2022-10-13 09:53:48 -0400

    Add playerselection & round Funtion

--- java2.0.js
+++ java2.0.js
@@ -1,23 +1,32 @@
-//function variables
-const playerChoice = getPlayerChoice(); 
-
-const computerChoice = getComputerChoice();
-
+let computerChoice=getComputerChoice();
+let playerChoice='';
 
 //Global Variable
 let playerScore=0;
 let computerScore=0;
-let gamesTied=0;
+let roundsPlayed=0;
+let result= '';
 
-
-
-
-console.log(game(playerScore, computerScore, gamesTied));
+//event listener for all
+const buttons = document.querySelectorAll('button');
+const Result=document.querySelector('#Result');
+const pScore=document.querySelector('#pScore');
+const cScore=document.querySelector('#cScore');
+const rndsPlayed=document.querySelector('#rndsPlayed');
 
 
 
 
 //Main Functions
+//Gets player choice & plays round
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerChoice=button.id;
+        playRound(playerChoice, computerChoice);
+    });
+});
+
 
     //computer choice function
 function getComputerChoice(){
@@ -25,17 +34,13 @@ function getComputerChoice(){
     const randomChoice = rpsArray[Math.floor(Math.random() * rpsArray.length)];
     return randomChoice;
 } 
-    //player choice function
-function getPlayerChoice(){
-let pSelection=prompt('Rock, paper, or scissors?: ');
-const playerSelection= pSelection.toLowerCase();
-return playerSelection;
-}
+
     //round function 
-function playRound(playerChoice, computerChoice){
+function playRound(playerChoice,computerChoice){
 if(playerChoice === computerChoice){
     let tied = "You Tied! Try again?";
     let result = tied;
+    console.log(playerChoice, computerChoice, result);
     return result;
     }
     else if((playerChoice === 'rock' && computerChoice === 'paper') || 
@@ -43,6 +48,8 @@ if(playerChoice === computerChoice){
     playerChoice === 'scissors' && computerChoice === 'rock'){
     let loss = "You Lose! " + computerChoice + " beats " + playerChoice;
     let result = loss;
+    computerScore++;
+    console.log(playerChoice, computerChoice, result);
     return result;
     }
     else if((playerChoice === 'rock' && computerChoice === 'scissors') 
@@ -50,44 +57,13 @@ if(playerChoice === computerChoice){
     ||  (playerChoice === 'scissors' && computerChoice === 'paper')){
     let win = "You Win! " + playerChoice + " beats " + computerChoice;
     let result = win;
+    playerScore++;
+    console.log(playerChoice, computerChoice, result);
     return result;
-    }
-}
-    //game function 
-function game(playerScore, computerScore, gamesTied){
-    for(let i = 0; i < 5 || i < 5; i++){
-        const playerChoice = getPlayerChoice();
-        const computerChoice = getComputerChoice();
-        const round = playRound(playerChoice, computerChoice);
-        console.log(playerChoice);
-        console.log(computerChoice);
-        console.log(round);
-    if(round === "You Win! " + playerChoice + " beats " + computerChoice){
-        playerScore++;
-    }else if(round === "You Lose! " + computerChoice + " beats " + playerChoice){
-        computerScore++;
-    }else{
-        gamesTied++;
-    }
-    }
-    if(playerScore > computerScore){
-        let endResult= "You Win! Try again?";
-        return {
-            endResult, gamesTied, playerScore, computerScore
-        };
-    }else if (playerScore < computerScore){
-        let endResult= "You Lose! Try again?";
-        return {
-            endResult, gamesTied, playerScore, computerScore
-        };
-    }else{
-        let endResult= "You Tied! Try again?";
-        return{
-            endResult, gamesTied, playerScore, computerScore
-        };
     }
     
 }
+
 
 
 
