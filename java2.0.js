091e056e6a3598fb53c6d2f5bcad7817fb98cb35
EthanSchultz <ethanschultz0224@gmail.com>
2022-10-13 15:44:11 -0400

    add runnign js as well as UI manipulation

--- java2.0.js
+++ java2.0.js
@@ -1,29 +1,31 @@
-let computerChoice=getComputerChoice();
+let computerChoice='';
 let playerChoice='';
 
 //Global Variable
+
 let playerScore=0;
 let computerScore=0;
 let roundsPlayed=0;
-let result= '';
+let result='';
 
-//event listener for all
+//UI Selectors/ manipulation
+
 const buttons = document.querySelectorAll('button');
 const Result=document.querySelector('#Result');
 const pScore=document.querySelector('#pScore');
 const cScore=document.querySelector('#cScore');
 const rndsPlayed=document.querySelector('#rndsPlayed');
 
-
-
-
 //Main Functions
-//Gets player choice & plays round
+//Gets player choice & runs playRound function
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerChoice=button.id;
-        playRound(playerChoice, computerChoice);
+        computerChoice=getComputerChoice();
+        playerImgMvr(playerChoice);
+        computerImgMvr(computerChoice);
+        setTimeout(playRound,250, playerChoice, computerChoice);
     });
 });
 
@@ -39,32 +41,59 @@ function getComputerChoice(){
 function playRound(playerChoice,computerChoice){
 if(playerChoice === computerChoice){
     let tied = "You Tied! Try again?";
-    let result = tied;
-    console.log(playerChoice, computerChoice, result);
-    return result;
+    result = tied;
+    roundsPlayed++;  
     }
     else if((playerChoice === 'rock' && computerChoice === 'paper') || 
     (playerChoice === 'paper' && computerChoice === 'scissors') || 
     playerChoice === 'scissors' && computerChoice === 'rock'){
     let loss = "You Lose! " + computerChoice + " beats " + playerChoice;
-    let result = loss;
+    result = loss;
     computerScore++;
-    console.log(playerChoice, computerChoice, result);
-    return result;
+    roundsPlayed++;
+   
     }
     else if((playerChoice === 'rock' && computerChoice === 'scissors') 
     ||  (playerChoice === 'paper' && computerChoice === 'rock') 
     ||  (playerChoice === 'scissors' && computerChoice === 'paper')){
     let win = "You Win! " + playerChoice + " beats " + computerChoice;
-    let result = win;
+    result = win;
     playerScore++;
-    console.log(playerChoice, computerChoice, result);
-    return result;
+    roundsPlayed++;
     }
-    
+    pScore.textContent=`Player Score: ${playerScore}`;
+    rndsPlayed.textContent=`Rounds Played: ${roundsPlayed}`;
+    cScore.textContent=`Computer Score: ${computerScore}`;
+    Result.textContent=result;
+
 }
+    //image functions
+function playerImgMvr(playerChoice){
+    if(playerChoice==='rock'){
+        document.getElementById('pChoice').src="./images/pRock.jpg";
+    }else if(playerChoice==='paper'){
+        document.getElementById('pChoice').src="./images/pPaper.jpg";
+    }else if(playerChoice==='scissors'){
+        document.getElementById('pChoice').src="./images/pScissors.jpg";
+    }
+}
+function computerImgMvr(computerChoice){
+    if(computerChoice==='rock'){
+        document.getElementById('comChoice').src="./images/cRock.jpg";
+    }else if(computerChoice==='paper'){
+        document.getElementById('comChoice').src="./images/cPaper.jpg";
+    }else if(computerChoice==='scissors'){
+        document.getElementById('comChoice').src="./images/cScissors.jpg";
+    }
+}
+//ends game/restarts game
+function checkScore(playerScore, computerScore){
+    if(playerScore === 5){
 
+    }else if(computerScore === 5){
 
+    }
+}
 
 
 
